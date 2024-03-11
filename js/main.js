c994098acdd67b4394f138e9c36f3914da97e70a
imanuelns <122876621+imanuelns@users.noreply.github.com>
2024-03-11 16:09:53 +0700

    add data layer events (#1)

--- js/main.js
+++ js/main.js
@@ -4,18 +4,18 @@ function main() {
 (function () {
    'use strict';
    
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 50
-            }, 900);
-            return false;
-          }
-        }
-      });
+  	// $('a.page-scroll').click(function() {
+    //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //       var target = $(this.hash);
+    //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //       if (target.length) {
+    //         $('html,body').animate({
+    //           scrollTop: target.offset().top - 50
+    //         }, 900);
+    //         return false;
+    //       }
+    //     }
+    //   });
 
 
     $('body').scrollspy({ 
