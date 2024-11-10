005b9e8f04ee23e1e4f5e01b6481c52d033050b6
imanuelns <122876621+imanuelns@users.noreply.github.com>
2024-11-11 01:28:11 +0700

    Tidying Up & POC of Datalayer (#18)
    * centralized gtm initialization
    
    * poc tracking id
    
    * check data layer
    
    * set target to blank for body button
    
    * poc datalayer
    
    * poc datalayer
    
    * poc datalayer
    
    * poc datalayer
    
    * trial add id for event
    
    * tidying up
    
    * tidying up

--- js/datalayer.js
+++ js/datalayer.js
@@ -1,11 +1,20 @@
 /* push datalayer */
 function pushWADataLayer(){
     dataLayer.push({
-        'event':'click',
-        'name':'WA button clicked',
+        'event':'click-wa',
+        'name':'WA button click',
+        'location':'body',
     });
 }
 
 /* set button events on whatsapp */
 $('.whatsapp-btn').on('click',pushWADataLayer);
 $('.whatsapp-btn-1').on('click',pushWADataLayer);
+
+$('.whatsapp-float').on('click',function(){
+    dataLayer.push({
+        'event':'click-wa',
+        'name':'WA button click',
+        'location':'float',
+    });
+});
